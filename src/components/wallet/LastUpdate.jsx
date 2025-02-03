40847e2afca4b769c90f42185566d5e9e300dc84
Theviv1 <vivstock29@gmail.com>
2025-02-03 12:04:51 +0100

    Delete src directory

--- src/components/wallet/LastUpdate.jsx
+++
@@ -1,9 +0,0 @@
-// function LastUpdate({ timestamp }) {
-//   return (
-//     <p className="text-sm text-gray-400">
-//       Last Updated: {new Date(timestamp).toLocaleString()}
-//     </p>
-//   );
-// }
-
-// export default LastUpdate;
